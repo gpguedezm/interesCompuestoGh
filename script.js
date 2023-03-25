49882631773cc601f14126350384159cb85c39c6
Giancarlos Guedez <gpguedezm@gmail.com>
2023-03-25 16:08:56 -0300

    aditando a español algunas etiquetas y variables para mejor entendimiento

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ form.addEventListener('submit', e => {
   e.preventDefault();
 
   let dinero = parseFloat(document.querySelector('#dinero').value);
-  let days = Number(document.querySelector('#days').value);
-  let rate = parseFloat(document.querySelector('#rate').value);
+  let dias = Number(document.querySelector('#dias').value);
+  let porcentaje = parseFloat(document.querySelector('#porcentaje').value);
 
-  if (isNaN(dinero) || isNaN(days) || isNaN(rate)) {
+  if (isNaN(dinero) || isNaN(dias) || isNaN(porcentaje)) {
     resultado.innerHTML = '<p id="error">Por favor ingrese valores numéricos en todos los campos.</p>';
   } else {
     let balance = parseFloat(dinero);
 
-    for (let i = 0; i < days; i++) {
-      const dailyRate = rate / 365;  // divide el porcentaje de ganancia en 365 días (un año)
-      const interes = parseFloat(balance * dailyRate / 100);
+    for (let i = 0; i < dias; i++) {
+      const porcentajeDiario = porcentaje / 365;  // divide el porcentaje de ganancia en 365 días (un año)
+      const interes = parseFloat(balance * porcentajeDiario / 100);
       //()
       balance += parseFloat(interes);
     }
@@ -24,7 +24,7 @@ form.addEventListener('submit', e => {
     ganancia = ganancia.toFixed(2);
 
     resultado.innerHTML = `
-      <p>El interés completo sobre ${dinero.toFixed(2)} pesos durante ${days} días al ${rate}% anual es:</p>
+      <p>El interés completo sobre ${dinero.toFixed(2)} pesos durante ${dias} días al ${porcentaje}% anual es:</p>
       <h2>Ganancia: ${ganancia} pesos</h2>
       <h2>Total: ${balance.toFixed(2)} pesos</h2>
     `;
